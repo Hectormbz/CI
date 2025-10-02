611b535e6e91d418338b03a68df0117d5948ce13
Hectormbz <hectorbzzctt@gmail.com>
2025-10-02 14:46:37 +0100

    commit

---
+++ test_ci.py
@@ -0,0 +1,11 @@
+import unittest
+from Functions import increment
+class TestFunction(unittest.TestCase):
+    
+    def test_Increment(self):
+        self.assertEqual(increment(5), 6)
+        self.assertEqual(increment(-8), -7)
+        self.assertEqual(increment(0), 1)
+
+if __name__ == '__main__':
+    unittest.main()
