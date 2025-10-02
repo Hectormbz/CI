f0a4131de371e82b153bc67353c47fe96c4b1af1
Hectormbz <hectorbzzctt@gmail.com>
2025-10-02 14:53:09 +0100

    commit

--- test_ci.py
+++ test_ci.py
@@ -3,7 +3,7 @@ from Functions import increment
 class TestFunction(unittest.TestCase):
     
     def test_Increment(self):
-        self.assertEqual(increment(5), 6)
+        self.assertEqual(increment(5), 7)
         self.assertEqual(increment(-8), -7)
         self.assertEqual(increment(0), 1)
 
