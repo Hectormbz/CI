9d329bc2698dd78fcf77aebfc48442c0d980e51a
Hectormbz <hectorbzzctt@gmail.com>
2025-10-07 22:24:27 +0100

    Accounting for outliers

--- graphGeneration.py
+++ graphGeneration.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+m = 2
+b = 5
+yerr = 20
 #load data from csv
 data = pd.read_csv("PipelineData.csv")
 x = data["x"].values
 y = data["y"].values
 
+#create set of data that ignores outliers:
+deviation = np.abs(y-(m*x+b))
+mask = deviation <= yerr
+xClean = x[mask]
+yClean = y[mask]
+
 #fit the line
-mFit, bFit = np.polyfit(x, y, 1)
-m = 2
-b = 5
+mFit, bFit = np.polyfit(xClean, yClean, 1)
+
 
 #plot data
 plt.scatter(x, y, label="Data")
 #adding error bars at twice starndard deviation of the noise
-plt.errorbar(x, mFit*x+bFit, yerr=20, fmt='none', ecolor='blue', capsize = 1, label = "error bars")
-#line of best fit
+plt.errorbar(x, m*x+b, yerr, fmt='none', ecolor='blue', capsize = 1, label = "error bars")
+#line of best fit using no outlying values
 plt.plot(x, mFit*x+bFit, color = "red", label = "fitted line", linestyle = "--")
 #original data 
 plt.plot(x, m*x+b, color = "green", label = "original data",)
